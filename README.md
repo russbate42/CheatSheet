f25d2e8a764f835bc305ecfa4146c4a04e1a4f7e
Russell Bate <r.bate@hotmail.com>
2024-03-18 14:14:12 +0100

    Working on getopts.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This is a compilation of useful commands plugged into your terminal!
 Feel free to fork this and make it your own! :-)
 
 ## GetOpt Info
+Useful descriptive articles:
+
+https://cplusplus.com/articles/DEN36Up4
+
 https://linux.die.net/man/3/getopt
 
 # Commands
--- cheatsheet.cxx
+++ cheatsheet.cxx
@@ -18,8 +18,26 @@ int getopt_long_only(int argc, char * const argv[],
                      const char *optstring,
                      const struct option *longopts, int *longindex);
 
+void PrintHelp()
+{
+    std::cout <<
+        "--users:             Show help related to users.\n";
+        "--help:              Show help\n";
+    exit(1);
+}
+
 int main(int argc, char* argv[]){
-	std::cout << "\nTest run!\n\n";
+//	std::cout << "\nTest run!\n\n";
+    
+	std::cout << std::endl;
+    std::cout << "Number of arguments: " << argc << std::endl;
+	std::cout << std::endl;
+
+    for (int i=0; i<argc; i++) {
+//	    std::cout << "\n" << input_var <<"\n\n";
+        std::string s(argv[i]); // char array to string
+        std::cout << "arg[" << i << "] = " << s << std::endl;
+    }
 	return 0;
 }
 
